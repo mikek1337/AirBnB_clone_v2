6e7fbf59079d54bc96f1c943c7186cbc3e5f294f
Mikek1337 <mikekegnlyew@gmail.com>
2023-01-04 11:50:02 +0300

    sql madness

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+CREATE USER IF NOT EXISTS hbnb_test IDENTIFIED BY 'hbnb_test_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db TO 'hbnb_test '@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
