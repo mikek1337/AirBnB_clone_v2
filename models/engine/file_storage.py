a3fec429d45fd37bc081da5cab9354b9dc392638
mikek1337 <mikekegnlyew@gmail.com>
2023-01-06 08:34:50 +0300

    fixed bug

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -56,7 +56,8 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        if obj != None:
+        if obj is not None:
             key = obj.__class__.__name__+'.'+obj.id
-            if key in self.__objects[key]:
-                del self.__objects[key]
+            del self.__objects[key]
+            """ if key in self.__objects[key]:
+                del self.__objects[key] """
