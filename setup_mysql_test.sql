e3dba1f3f5b5771c826995bb567cdda9e8b3cd44
mikek1337 <mikekegnlyew@gmail.com>
2023-01-06 20:58:13 +0300

    fix

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
-CREATE USER IF NOT EXISTS hbnb_test IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_test_db TO 'hbnb_test '@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
