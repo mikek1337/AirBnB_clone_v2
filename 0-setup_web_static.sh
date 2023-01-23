2a948d8a23961f9eca303770f21d7a0b360d1c56
mikek1337 <mikekegnlyew@gmail.com>
2023-01-23 20:25:37 +0300

    server config

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,3 +17,26 @@ sudo mkdir /data/web_static/releases/test > /dev/null 2>&1
 
 sudo touch /data/web_static/releases/test/index.html && echo "Hello world!" > /data/web_static/releases/test/index.html
 
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+server_config="server {
+                listen 80 ;
+                listen [::]:80 default_server;
+                root /data/web_static/current;
+                index index.html index.htm index.nginx-debian.html
+                server_name_ mikejourney.tech;
+                location / {
+                        try_files $uri $uri/ =404;
+                }
+                if ($request_filename ~ redirect_me){
+                        rewrite ^ https://mikejourney.tech/ permanent;
+                }
+                error_page 404 /error_404.html;
+                location = /error_404.html {
+                        internal;
+                }
+}"
+
+sudo echo $server_config >> /etc/nginx/sites-available/default
