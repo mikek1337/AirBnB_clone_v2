da611549488f92703e0e1c0ff28418cbd7faa5c1
mikek1337 <mikekegnlyew@gmail.com>
2023-01-23 22:38:54 +0300

    test

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,7 +8,7 @@ def do_pack():
     local("git clone https://github.com/mikek1337/AirBnb_clone_v2.git")
     local("cd ./AirBnb_clone_v2")
     local("mkdir versions > /dev/null 2>&1")
-    local("cd versions")
-    test = local("sudo tar -c -f -z -v web_static_{year}{month}{day}{hour}{minute}{second}.tgz ../web_static".format(year=datetime.datetime.today().year, month=datetime.datetime.today(
+
+    test = local("sudo tar -c -f -z -v versions/web_static_{year}{month}{day}{hour}{minute}{second}.tgz web_static".format(year=datetime.datetime.today().year, month=datetime.datetime.today(
     ).month, day=datetime.datetime.today().day, hour=datetime.datetime.today().hour, minute=datetime.datetime.today().minute, second=datetime.datetime.today().second))
     print(test)
