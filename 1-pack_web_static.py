d4c0d06db0957b3f81b978a92026f3130bbe8d45
mikek1337 <mikekegnlyew@gmail.com>
2023-01-23 22:49:48 +0300

    please work

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,6 +8,6 @@ def do_pack():
 
     local("mkdir versions > /dev/null 2>&1")
 
-    test = local("sudo tar -c -f -z -v versions/web_static_{year}{month}{day}{hour}{minute}{second}.tgz ./web_static".format(year=datetime.datetime.today().year, month=datetime.datetime.today(
+    test = local("sudo tar -cvf versions/web_static_{year}{month}{day}{hour}{minute}{second}.tgz ./web_static".format(year=datetime.datetime.today().year, month=datetime.datetime.today(
     ).month, day=datetime.datetime.today().day, hour=datetime.datetime.today().hour, minute=datetime.datetime.today().minute, second=datetime.datetime.today().second))
     print(test)
