ab6a2e35ff9e4e0f470c1af8120de2aaf7928895
mikek1337 <mikekegnlyew@gmail.com>
2023-01-23 23:01:48 +0300

    final

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,13 +1,21 @@
+#!/usr/bin/python3
+
+"""create archive."""
+
 from fabric.api import local, env
 import datetime
-
+from os.path import isdir
 env.hosts = "127.0.0.1"
 
 
 def do_pack():
-
-    local("mkdir versions > /dev/null 2>&1")
-
-    test = local("sudo tar -cvf versions/web_static_{year}{month}{day}{hour}{minute}{second}.tgz ./web_static".format(year=datetime.datetime.today().year, month=datetime.datetime.today(
-    ).month, day=datetime.datetime.today().day, hour=datetime.datetime.today().hour, minute=datetime.datetime.today().minute, second=datetime.datetime.today().second))
-    print(test)
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
