61d21de2667ae3c91b17787c18732056709ee6b5
mikek1337 <mikekegnlyew@gmail.com>
2023-01-23 22:17:46 +0300

    test

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,13 +1,13 @@
-from fabric.api import run, env
+from fabric.api import local, env
 from datetime import datetime
 
 env.hosts = "127.0.0.1"
 
 
 def do_pack():
-    run("sudo git clone https://github.com/mikek1337/AirBnb_clone_v2.git")
-    run("cd ./AirBnb_clone_v2")
-    run("mkdir versions > /dev/null 2>&1")
-    test = run("tar -c -f -z -v versions/web_static_{year}{month}{day}{hour}{minute}{second}.tgz".format(year=datetime.year(
+    local("sudo git clone https://github.com/mikek1337/AirBnb_clone_v2.git")
+    local("cd ./AirBnb_clone_v2")
+    local("mkdir versions > /dev/null 2>&1")
+    test = local("tar -c -f -z -v versions/web_static_{year}{month}{day}{hour}{minute}{second}.tgz".format(year=datetime.year(
     ), month=datetime.month(), day=datetime.day(), hour=datetime.hour(), minute=datetime.minute(), second=datetime.second()))
     print(test)
