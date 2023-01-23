b864aa4c57215e7028313719ea29926bd1a8a4a9
mikek1337 <mikekegnlyew@gmail.com>
2023-01-23 22:06:09 +0300

    pack folder

---
+++ 1-pack_web_static.py
@@ -0,0 +1,12 @@
+from fabric.api import run, env
+from datetime import datetime
+env.host = 'localhost'
+
+
+def do_pack():
+    run("git clone https://github.com/mikek1337/AirBnb_clone_v2.git")
+    run("cd ./AirBnb_clone_v2")
+    run("mkdir versions > /dev/null 2>&1")
+    test = run("tar -c -f -z -v versions/web_static_{year}{month}{day}{hour}{minute}{second}.tgz".format(year=datetime.year(
+    ), month=datetime.month(), day=datetime.day(), hour=datetime.hour(), minute=datetime.minute(), second=datetime.second()))
+    print(test)
