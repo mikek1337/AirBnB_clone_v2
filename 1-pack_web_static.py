bba95af54820cb51bf90f366711df80b5588d874
mikek1337 <mikekegnlyew@gmail.com>
2023-01-23 22:36:38 +0300

    this fix problem

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,6 +9,6 @@ def do_pack():
     local("cd ./AirBnb_clone_v2")
     local("mkdir versions > /dev/null 2>&1")
     local("cd versions")
-    test = local("tar -c -f -z -v web_static_{year}{month}{day}{hour}{minute}{second}.tgz".format(year=datetime.datetime.today().year, month=datetime.datetime.today(
+    test = local("sudo tar -c -f -z -v web_static_{year}{month}{day}{hour}{minute}{second}.tgz ../web_static".format(year=datetime.datetime.today().year, month=datetime.datetime.today(
     ).month, day=datetime.datetime.today().day, hour=datetime.datetime.today().hour, minute=datetime.datetime.today().minute, second=datetime.datetime.today().second))
     print(test)
