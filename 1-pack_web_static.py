f095aa520bc1fa3f3387bb4ba9ca8070662abb95
mikek1337 <mikekegnlyew@gmail.com>
2023-01-23 22:14:27 +0300

    test

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,7 +5,7 @@ env.hosts = "127.0.0.1"
 
 
 def do_pack():
-    run("git clone https://github.com/mikek1337/AirBnb_clone_v2.git")
+    run("sudo git clone https://github.com/mikek1337/AirBnb_clone_v2.git")
     run("cd ./AirBnb_clone_v2")
     run("mkdir versions > /dev/null 2>&1")
     test = run("tar -c -f -z -v versions/web_static_{year}{month}{day}{hour}{minute}{second}.tgz".format(year=datetime.year(
