d449b7c6886cd142e29f18b2e2c2fd19da4389d2
mikek1337 <mikekegnlyew@gmail.com>
2023-02-05 09:04:11 +0300

    python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""# using url parameter."""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """# index route."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """# hbnb route."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """# C route."""
+    text = str(text)
+    text = text.split("_")
+    text = " ".join(text)
+    return "C " + text
+
+
+@app.route("/python/", defaults={'text': 'is cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """# Python route."""
+    text = str(text)
+    text = text.split("_")
+    text = " ".join(text)
+    return "Python " + text
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
