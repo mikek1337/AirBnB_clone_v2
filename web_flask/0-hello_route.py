e06e2f3d64a19aab4e6be9f5c61fea81f1abdc6f
mikek1337 <mikekegnlyew@gmail.com>
2023-02-05 07:44:39 +0300

    multiple route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-"""First flask route."""
+"""# First flask route."""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    """Display Hello HBNB."""
+    """# Display Hello HBNB."""
     return "Hello HBNB!"
 
 
