875f6776da12ac73ef3210f1d0b3080f64dee4e7
mikek1337 <mikekegnlyew@gmail.com>
2023-02-05 08:03:51 +0300

    c is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""# using url parameter."""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """# index route."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """# hbnb route."""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """# C route."""
+    text = str(text)
+    text = text.split("_")
+    text = "  ".join(text)
+    return "C " + text
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
