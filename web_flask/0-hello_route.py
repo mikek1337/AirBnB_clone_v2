ad3192f5e9e68778203f1dbd4ea79e098610cadb
mikek1337 <mikekegnlyew@gmail.com>
2023-02-05 06:57:54 +0300

    flask fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 """First flask route."""
-
-from flask import Flask
-app = Flask(__name__)
-
+from web_flask import app
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
