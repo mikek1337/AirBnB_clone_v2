9b914384143ff8bd17a073aac2a4de440875d0a6
mikek1337 <mikekegnlyew@gmail.com>
2023-03-08 23:16:01 +0300

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,16 +2,14 @@
 
 """# First flask route."""
 from flask import Flask
-from gevent.pywsgi import WSGIServer
 app = Flask(__name__)
 
 
-@app.route('/airbnb-onepage', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def index():
     """# Display Hello HBNB."""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    app.run(host='0.0.0.0', port='5000')
